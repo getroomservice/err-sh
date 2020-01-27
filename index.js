f2fa524097b48b13ff0c9e3a31d934f5d205eb87
Flaque <evanjamesconrad@gmail.com>
2020-01-27 13:18:08 -0800

    ZEIT => Room Service

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 module.exports = (request, response) => {
-  const { url } = request
-  const urlParts = url.split('/')
+  const { url } = request;
+  const urlParts = url.split("/");
 
-  if (url === '/') {
+  if (url === "/") {
     response.writeHead(302, {
-      Location: `https://zeit.co/blog/err-sh`
-    })
+      Location: `https://www.roomservice.dev`
+    });
 
-    response.end()
-    return
+    response.end();
+    return;
   }
 
   if (urlParts.length < 3) {
     return {
-      error: 'Please specify all the missing data (see the repo)!',
-      errorHandle: 'missing_data'
-    }
+      error: "Please specify all the missing data (see the repo)!",
+      errorHandle: "missing_data"
+    };
   }
 
-  const isZeit = urlParts.length === 3
-  const user = isZeit ? 'zeit' : urlParts[1]
-  const repo = urlParts[isZeit ? 1 : 2]
-  const code = urlParts[isZeit ? 2 : 3]
+  const isUs = urlParts.length === 3;
+  const user = isUs ? "getroomservice" : urlParts[1];
+  const repo = urlParts[isUs ? 1 : 2];
+  const code = urlParts[isUs ? 2 : 3];
 
   response.writeHead(302, {
     Location: `https://github.com/${user}/${repo}/blob/master/errors/${code}.md`
-  })
+  });
 
-  response.end()
-}
+  response.end();
+};
